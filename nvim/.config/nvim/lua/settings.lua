b093f2887f4af87f97ea3545333d424647646fe5
Root User <=>
2024-02-18 07:16:30 -0500

    update nvim settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -14,8 +14,6 @@ o.backupdir = HOME .. '/vim/backup'
 o.directory = HOME .. '/vim/tmp'
 o.switchbuf = 'vsplit'
 
-o.tabstop = 4    --Tells vim the number of columns a tab should be counted as
-o.expandtab = true    --A tab in insert mode is converted to spaces.
 -- Have a backup file created - file_name~ will be created, which is a capture
 -- of the file prior to the current edit.
 o.backup = true
@@ -27,22 +25,14 @@ o.history = 50 --Keep 50 lines of history.
 o.wildmode = 'longest,list,full' --Similar to bash autocomplete.
 o.wildmenu = true
 o.cindent = true
-o.autoindent = true
 o.background = 'dark'
 
-o.shiftwidth = 4 --Tells vim the number of columns a shiftwidth should be counted as
-o.smarttab = true     --Will delete and insert shiftwidth, rather than one space
-o.autoindent = true   --Copies indent from the current line to the next line
-o.hlsearch = true   --highlight keywords being matched from search.
-o.incsearch = true  --When you beging typing a a word to search, vi(m) will begin
-                --matchig incrementally rather than waiting until completion.
 o.backspace = 'indent,eol,start' --How backspace behaves
 o.selectmode = 'mouse' --How selection behaves while selecting with a mouse
 o.mousemodel='extend' -- Pressing <RightMouse> extends selection instead of showing popup-menu: >vim
 o.modeline = true
 o.showcmd = true --Display what is currently typed in the bottom right hand corner
 o.showmatch = true --highlight matching parenthesis.
-o.cursorline = true   --See that beautiful line currently highlighting where your cursor is
 o.mouse = 'a'
 --o.ttymouse = 'xterm2'
 
@@ -50,9 +40,36 @@ o.mouse = 'a'
 --o.makeprg = 'cv'
 
 --[[ START AUTOCOMPLETE ]]--
-o.completeopt = 'menu,menuone,longest'
+--o.completeopt = 'menu,menuone,longest'
+o.completeopt = {'menu', 'menuone', 'noselect'}
 o.complete = '.,w,b,u,t'
 vim.cmd([[
 set shortmess+=c " do not display redundant info in the status bar when menu pops up
 ]])
 --[[ END AUTOCOMPLETE ]]--
+
+o.clipboard = 'unnamedplus'   -- use system clipboard 
+
+-- Tab
+o.tabstop = 4                 -- number of visual spaces per TAB
+o.softtabstop = 4             -- number of spacesin tab when editing
+o.shiftwidth = 4              -- insert 4 spaces on a tab
+o.expandtab = true            -- tabs are spaces, mainly because of python
+o.smarttab = true     --Will delete and insert shiftwidth, rather than one space
+o.autoindent = true   --Copies indent from the current line to the next line
+
+-- UI config
+--o.number = true               -- show absolute number
+--o.relativenumber = true       -- add numbers to each line on the left side
+o.cursorline = true           -- highlight cursor line underneath the cursor horizontally
+o.splitbelow = true           -- open new vertical split bottom
+o.splitright = true           -- open new horizontal splits right
+-- o.termguicolors = true        -- enabl 24-bit RGB color in the TUI
+o.showmode = false            -- we are experienced, wo don't need the "-- INSERT --" mode hint
+
+-- Searching
+o.incsearch = true            -- search as characters are entered
+o.hlsearch = true             --highlight keywords being matched from search.
+o.ignorecase = true           -- ignore case in searches by default
+o.smartcase = true            -- but make it case sensitive if an uppercase is entered
+
