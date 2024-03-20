9bd8f0ae973fc581e1f36a78a7f547da3a57e749
Mike Koldychev <mkoldych@ciena.com>
2024-03-20 16:09:56 -0400

    lualine

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -12,8 +12,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
+  { "nvim-tree/nvim-web-devicons" },
+--  { "kyazdani42/nvim-web-devicons" },
   { "nvim-lua/plenary.nvim" },
-  { "nvim-telescope/telescope-fzf-native.nvim", run = 'make'  },
+--  { "nvim-telescope/telescope-fzf-native.nvim",
+--    config = 'make' },
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { 
@@ -27,15 +30,13 @@ local plugins = {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    run = function()
+    config = function()
         local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
         ts_update()
     end,
   },
-  { "nvim-tree/nvim-web-devicons" },
   { "preservim/tagbar" },
 
-  { "kyazdani42/nvim-web-devicons" },
   { "kyazdani42/nvim-tree.lua" },
 
   -- Colorschemes
@@ -136,6 +137,57 @@ local plugins = {
           map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
           print('hello')
         end
+      }
+    end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          }
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            },
+          },
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
       }
     end,
   },
