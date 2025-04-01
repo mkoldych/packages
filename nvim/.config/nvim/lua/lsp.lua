bb1447c0a719c18e8e308e95b57ec4c0fb14cfb0
Mike Koldychev <mkoldych@ciena.com>
2025-04-01 12:52:27 -0400

    option to manually start pyright

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -40,7 +40,70 @@ lsp.ccls_start = function ()
                   virtual_text = false,
                   signs = true,
                   update_in_insert = false,
-                  underline = true,
+                  underline = false
+                }
+            )
+        }
+    })
+end
+
+function pyright_root_dir (cur_file)
+    root_dir = vim.fs.dirname(
+        vim.fs.find(
+            {'pyrightconfig.json', '.git'},
+            { upward = true, path = vim.fs.dirname(cur_file) }
+        )[1]
+    )
+    if root_dir ~= nil then
+        return root_dir
+    end
+    root_dir = vim.fs.find(
+        {'site-packages'},
+        { upward = true, path = vim.fs.dirname(cur_file) }
+    )[1]
+    return root_dir
+end
+
+lsp.pyright_start = function ()
+    local cur_file = vim.api.nvim_buf_get_name(0)
+    local ok, root_dir = pcall(pyright_root_dir, cur_file)
+    if not ok then
+        print('no pyrightconfig file, error: ' .. root_dir)
+        return
+    elseif root_dir == nil then
+        print('no pyrightconfig file, from ' .. cur_file)
+        return
+    else
+        --print('found pyrightconfig file at ' .. root_dir)
+    end
+    vim.lsp.start({
+        name = 'pyright-langserver',
+        cmd = {'/usr/local/bin/pyright-langserver', '--stdio'},
+        autostart = false,
+        root_dir = root_dir,
+        --https://microsoft.github.io/pyright/#/settings?id=pyright-settings
+        settings =  {
+		  python = {
+			analysis = {
+              extraPaths={"/corp/sta/opt/Python-3.8/lib/python3.8/site-packages"},
+              logLevel="Information",
+              typeCheckingMode="strict",
+			  autoSearchPaths = true,
+			  diagnosticMode = "openFilesOnly",
+			  useLibraryCodeForTypes = true
+			}
+		  }
+		},
+        handlers = {
+            ["textDocument/publishDiagnostics"] = vim.lsp.with(
+                vim.lsp.diagnostic.on_publish_diagnostics,
+                {
+                  virtual_text = true,
+                  signs = {
+                      priority=1
+                  },
+                  update_in_insert = false,
+                  underline = false,
                 }
             )
         }
@@ -49,9 +112,6 @@ end
 
 lsp.setup = function ()
     -- Setup language servers.
-    local lspconfig = require('lspconfig')
-    lspconfig.pyright.setup {}
-
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('UserLspConfig', {}),
       callback = function(ev)
@@ -63,10 +123,21 @@ lsp.setup = function ()
       end,
     })
 
+    vim.lsp.set_log_level('info')
+
     vim.cmd [[
         augroup ccls_start_aug
           autocmd!
           autocmd FileType c,cpp,h,hpp lua require('lsp').ccls_start()
+        augroup END
+    ]]
+
+    --local lspconfig = require('lspconfig')
+    --lspconfig.pyright.setup {}
+    vim.cmd [[
+        augroup pyright_start_aug
+          autocmd!
+          autocmd FileType python lua require('lsp').pyright_start()
         augroup END
     ]]
 end
